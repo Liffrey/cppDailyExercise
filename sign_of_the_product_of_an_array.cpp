310e95847947b83a75f55dde3d03072b7aad7b11
Liffrey <bugrauvez@gmail.com>
2022-09-27 01:11:49 +0300

    27092022

--- sign_of_the_product_of_an_array.cpp
+++ sign_of_the_product_of_an_array.cpp
@@ -9,7 +9,6 @@
 
 // Return signFunc(product).
 
- 
 // Example 1:
 
 // Input: nums = [-1,-2,-3,-4,3,2,1]
@@ -27,10 +26,60 @@
 // Input: nums = [-1,1,-1,1,-1]
 // Output: -1
 // Explanation: The product of all values in the array is -1, and signFunc(-1) = -1
- 
+
 // Constraints:
 
 // 1 <= nums.length <= 1000
 // -100 <= nums[i] <= 100
 
-//level:easy
+// level:easy
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int SignBit(int x)
+{
+    int result = 128 & x;
+    if (result == 128) return -1;
+    else return 1;
+}
+class Solution
+{
+public:
+    int arraySign(vector<int>& nums)
+    {
+        // sayı negatif, pozitif veya sıfır mı?
+        int N = nums.size();
+        int val = 0;
+        int multiply = 1;
+        // tüm sayıların üstünden geç
+        for (int i = 0; i < N; i++)
+        {
+            if (nums[i] == 0)
+                return 0;
+
+            val = SignBit(nums[i]);
+            multiply *= val;
+        }
+        return multiply;
+    }
+};
+
+int main()
+{
+    int result = 0;
+    vector<int> num3{ -1, -2, -3, -4, 3, 2, 1 };
+    vector<int> num2{ 1, 5, 0, 2, -3 };
+    vector<int> num1{ -1, 1, -1, 1, -1 };
+    vector<vector<int>> nums{ num3, num2, num1 };
+    vector<int> expected{ 1, 0, -1 };
+    Solution test{};
+    for (int i = 0; i < nums.size(); i++)
+    {
+        result = test.arraySign(nums[i]);
+        cout << "Beklenen->" << expected[i] << "   "
+            << "Sonuc->" << result << endl;
+    }
+    return 0;
+}
